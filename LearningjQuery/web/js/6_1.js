a173640ead4b4ec8eda8ac32f47c0ca8d0f347e1
Cowerling <Cowerling@gmail.com>
2016-06-06 17:27:51 +0800

    Sending Data with Ajax at workspace

--- LearningjQuery/web/js/6_1.js
+++ LearningjQuery/web/js/6_1.js
@@ -5,7 +5,20 @@
 $(document).ready(function () {
     $("#letter-a a").click(function (event) {
         event.preventDefault();
-        $("#dictionary").load("6_a.html");
+        /*$("#dictionary").hide().load("6_a.html", function () {
+            $(this).fadeIn();
+        });*/
+        $.ajaxSetup({
+            url: "6_a.html",
+            type: "POST",
+            dataType: "html"
+        });
+        $.ajax({
+            type: "GET",
+            success: function (data) {
+                $("#dictionary").hide().html(data).fadeIn();
+            }
+        });
     });
 });
 
@@ -82,7 +95,12 @@ $(document).ready(function () {
         /*$.post("GetDictionaryTerm", requestData, function (data) {
             $("#dictionary").html(data);
         });*/
-        $("#dictionary").load("GetDictionaryTerm", requestData);
+        /*$("#dictionary").load("GetDictionaryTerm", requestData);*/
+        $.get("GetDictionaryTerm", requestData, function (data) {
+            $("#dictionary").html(data);
+        }).fail(function (jqXHR) {
+            $("#dictionary").html("An error occured: " + jqXHR.status).append(jqXHR.responseText); 
+        });
     });
 });
 
@@ -92,6 +110,52 @@ $(document).ready(function () {
         var formValues = $(this).serialize();
         $.get("GetDictionaryTerm", formValues, function (data) {
             $("#dictionary").html(data);
+        });
+    });
+});
+
+$(document).ready(function () {
+    var $loading = $("<div id='loading'>Loading...</div>").insertBefore("#dictionary");
+    $(document).ajaxStart(function () {
+        $loading.show();
+    }).ajaxStop(function () {
+        $loading.hide();
+    });
+});
+
+$(document).ready(function () {
+    $("body").on("click", "h3.term", function () {
+        $(this).siblings(".definition").slideToggle();
+    });
+});
+
+$(document).ready(function () {
+    var url = "http://examples.learningjquery.com/jsonp/g.php";
+    $("#letter-g a").click(function (event) {
+        event.preventDefault();
+        $.getJSON(url + "?callback=?", function (data) {
+            var html = "";
+            $.each(data, function (entryIndex, entry) {
+                html += "<div class='entry'>";
+                html += "<h3 class='term'>" + entry.term + "</h3>";
+                html += "<div class='part'>" + entry.part + "</div>";
+                html += "<div class='definition'>" + entry.definition;
+                if (entry.quote) {
+                    html += "<div class='quote'>";
+                    $.each(entry.quote, function (lineIndex, line) {
+                        html += "<div class='quote-line'>" + line + "</div>";
+                    });
+                    if (entry.author) {
+                        html += "<div class='quote-author'>" + entry.author + "</div>";
+                    }
+                    html += "</div>";
+                }
+                html += "</div>";
+                html += "</div>";
+            });
+            $("#dictionary").html(html);
+        }).fail(function (jqXHR) {
+            $("#dictionary").html("An error occured: " + jqXHR.status).append(jqXHR.responseText);
         });
     });
 });
